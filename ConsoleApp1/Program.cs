e59d6f9a85238a30dd0d5a0de8a29d63f390cb73
Mendaruiz <davodido@hotmail.com>
2021-05-19 09:05:13 +0200

    Initial Commit
    Proceso de viaje

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Dictionary<string, List<int>> diccionario = new Dictionary<string, List<int>>();
+
+            //Entrada de Datos.
+            List<string> cities = new List<string> { "Logroño", "Zaragoza", "Teruel", "Madrid", "Lleida", "Alicante", "Castellón", "Segovia", "Ciudad Real" };
+            List<List<int>> connections = new List<List<int>> {
+                new List<int>{0,4,6,8,0,0,0,0,0},
+                new List<int>{4,0,2,0,2,0,0,0,0},
+                new List<int>{6,2,0,3,5,7,0,0,0},
+                new List<int>{8,0,3,0,0,0,0,0,0},
+                new List<int>{0,2,5,0,0,0,4,8,0},
+                new List<int>{0,0,7,0,0,0,3,0,7},
+                new List<int>{0,0,0,0,4,3,0,0,6},
+                new List<int>{0,0,0,0,8,0,0,0,4},
+                new List<int>{0,0,0,0,0,7,6,4,0}
+            };
+            string ciudadOrigen = "Logroño"; //Ciudad Real
+            string ciudadDestino = "Ciudad Real";//Logroño
+            Dictionary<string, int> listaOptima = new Dictionary<string, int>();
+            Viaje viaje = new Viaje(cities, connections);
+            Viaje.ObtenerRutaOptima(ciudadOrigen, ciudadDestino, listaOptima, ciudadOrigen, 0);
+            Console.WriteLine($"La Ciudad {ciudadOrigen} con ruta a {ciudadDestino}, tiene la siguiente ruta {listaOptima.First().Key}, con un coste de {listaOptima.First().Value}");
+            Dictionary<string, int> listaRutasCiudad = new Dictionary<string, int>();
+            listaRutasCiudad = Viaje.DevolverRutasCiudades(ciudadOrigen);
+            Console.WriteLine($"La Ciudad {ciudadOrigen}, tiene la siguientes rutas:");
+            foreach (KeyValuePair<string,int> ruta in listaRutasCiudad)
+            {
+                Console.WriteLine($"{ruta.Key}, con un coste de {ruta.Value}");
+            }
+            Console.WriteLine($"La Ciudad {ciudadOrigen} con ruta a {ciudadDestino}, tiene la siguiente ruta {listaOptima.First().Key}, con un coste de {listaOptima.First().Value}");
+
+        }
+    }
+}
---
+++ ConsoleApp1/Viaje.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Viaje
+    {
+        private static List<string> mCities { get; set; }
+
+        private static List<List<int>> mConnections { get; set; }
+        private static List<string> mRoutes { get; set; }
+
+        public Viaje(List<string> pCities, List<List<int>> pConnections)
+        {
+            mCities = pCities;
+            mConnections = pConnections;
+        }
+
+        private static void ComprobarCiudad(string pCiudad)
+        {
+            if (!mCities.Contains(pCiudad))
+            {
+                throw new Exception("La Ciudad que esta intentando introducir no existe");
+            }
+        }
+
+        public static Dictionary<string, int> DevolverRutasCiudades(string pCiudadOrigen)
+        {
+            Dictionary<string, int> rutasDevolver = new Dictionary<string, int>();
+            ComprobarCiudad(pCiudadOrigen);
+            foreach (string ciudad in mCities)
+            {
+                if (!ciudad.Equals(pCiudadOrigen))
+                {
+                    Dictionary<string, int> ListaDestinoOptimo = new Dictionary<string, int>();
+                    ObtenerRutaOptima(pCiudadOrigen, ciudad, ListaDestinoOptimo, pCiudadOrigen, 0);
+                    if (ListaDestinoOptimo.Count() > 0 )
+                    {
+                        rutasDevolver.TryAdd(ListaDestinoOptimo.First().Key, ListaDestinoOptimo.First().Value);
+                    }                  
+                }
+            }
+            return rutasDevolver;
+        }
+
+        public static bool ObtenerRutaOptima(string pCiudadOrigen, string pCiudadDestino, Dictionary<string, int> pListaDestinos, string pOrigen, int pDistancia)
+        {
+            ComprobarCiudad(pCiudadOrigen);
+            ComprobarCiudad(pCiudadDestino);
+            bool volver = false;
+            List<string> listaPosiblesDestinos = RutasPosiblesDeCiudad(pCiudadOrigen);
+            if (listaPosiblesDestinos.Contains(pCiudadDestino))
+            {
+                pDistancia += DevolverDistancia(pCiudadOrigen, pCiudadDestino);
+                if(pListaDestinos.Keys.Count == 0)
+                {
+                    pListaDestinos.TryAdd(pOrigen + "," + pCiudadDestino, pDistancia);
+                }
+                else
+                {
+                    int distancia = pListaDestinos.First().Value;
+                    if(distancia > pDistancia)
+                    {
+                        pListaDestinos.Clear();
+                        pListaDestinos.TryAdd(pOrigen + "," + pCiudadDestino, pDistancia);
+                    }
+                }
+                volver = true;
+            }
+            else
+            {
+                foreach (string ciudad in listaPosiblesDestinos)
+                {
+                    if (!pOrigen.Contains(ciudad))
+                    {
+                        pDistancia += DevolverDistancia(pCiudadOrigen, ciudad);
+                        if (!ObtenerRutaOptima(ciudad, pCiudadDestino, pListaDestinos, pOrigen + "," + ciudad, pDistancia))
+                        {
+                            pDistancia -= DevolverDistancia(pCiudadOrigen, ciudad);
+                        }
+                    }
+                }
+            }
+            return volver;
+        }
+
+
+        public static void ObtenerPosiblesRutas(string pCiudadOrigen, string pCiudadDestino, List<string> pListaDestinos, string pOrigen)
+        {
+            List<string> listaPosiblesDestinos = RutasPosiblesDeCiudad(pCiudadOrigen);
+            if (listaPosiblesDestinos.Contains(pCiudadDestino))
+            {
+                pListaDestinos.Add(pOrigen + "," + pCiudadDestino);
+            }
+            else
+            {
+                foreach(string ciudad in listaPosiblesDestinos)
+                {
+                    if (!pOrigen.Contains(ciudad))
+                    {
+                        ObtenerPosiblesRutas(ciudad, pCiudadDestino, pListaDestinos, pOrigen + "," + ciudad);
+                    }
+                }
+            }
+        }
+
+        private static bool TieneConexion(List<string> pCities, List<List<int>> pConnections, string pCiudadOrigen, string pCudadDestino)
+        {
+            bool tieneConexion = false;
+            if (pConnections[pCities.IndexOf(pCiudadOrigen)][pCities.IndexOf(pCudadDestino)] != 0)
+            {
+                tieneConexion = true;
+            }
+            return tieneConexion;
+        }
+
+        private static int DevolverConexion(List<string> pCities, List<List<int>> pConnections, string pInicio, string pFin)
+        {
+            int conexion = 0;
+            if (pConnections[pCities.IndexOf(pInicio)][pCities.IndexOf(pFin)] != 0)
+            {
+                conexion = pConnections[pCities.IndexOf(pInicio)][pCities.IndexOf(pFin)];
+            }
+            return conexion;
+        }
+
+        private static int DevolverDistancia(string pCiudadOrigen, string pCudadDestino)
+        {
+            return mConnections[mCities.IndexOf(pCiudadOrigen)][mCities.IndexOf(pCudadDestino)];
+        }
+
+        private static List<string> RutasPosiblesDeCiudad (string pCiudad)
+        {
+            List<string> rutas = new List<string>();
+            foreach (string ciudad in mCities)
+            {
+                if (pCiudad != ciudad)
+                {
+                    if(DevolverDistancia(pCiudad, ciudad) > 0)
+                    {
+                        rutas.Add(ciudad);
+                    }
+                }
+            }
+            return rutas;
+        }
+       
+    }
+}
